9c46dcf9231d4605a4d68ba0df7791a4e0f1036e
Paul Denlinger <pmdenlinger@icloud.com>
2025-11-25 11:57:48 -0800

    First commit. Add VStack and text, text modifiers.

---
+++ SwiftUI Basics/SwiftUI Basics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI Basics
+//
+//  Created by Paul Denlinger on 11/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("SwiftUI Basics")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(.blue)
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI Basics/SwiftUI Basics/SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicsApp.swift
+//  SwiftUI Basics
+//
+//  Created by Paul Denlinger on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
