a26df252028b71eb0b2ac2a079a428b602d6bd28
Paul Denlinger <pmdenlinger@icloud.com>
2025-11-25 12:49:46 -0800

    Animate screenTapped()

--- SwiftUI Basics/SwiftUI Basics/ContentView.swift
+++ SwiftUI Basics/SwiftUI Basics/ContentView.swift
@@ -10,18 +10,18 @@ import SwiftUI
 struct ContentView: View {
     
     let colorList1: [Color] = [.red, .purple, .green, .pink]
-    
     let colorList2: [Color] = [.black, .white, .blue]
     
-    @State var screenTapped: Bool = false
+    @State private var screenTapped: Bool = false
+    @State private var offsetY: CGFloat = -150.0
     
+    // Computed property
     var colors: [Color] {
         screenTapped ? colorList1 : colorList2
     }
     
     var body: some View {
         ZStack {
-
             LinearGradient(
                 colors: colors,
                 startPoint: .topLeading,
@@ -29,17 +29,25 @@ struct ContentView: View {
             )
             .opacity(0.8)
             .ignoresSafeArea()
-//             Text
+            
+            // Text
             Text("SwiftUI Basics")
                 .font(.largeTitle)
                 .fontWeight(.semibold)
                 .foregroundStyle(.black)
-            
+                .offset(y: offsetY)
         }
         .onTapGesture {
-//            Change screen color if tapped
+            // Change screen color if tapped
             screenTapped.toggle()
-            
+            withAnimation(.easeIn(duration: 0.5)) {
+                if screenTapped {
+                    offsetY = -1000
+                } else {
+                    offsetY = 0
+                }
+                
+            }
         }
     }
 }
