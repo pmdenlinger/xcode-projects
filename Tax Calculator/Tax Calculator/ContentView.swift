33a508f06847691c79ff5885889fe0203b279ca3
Paul Denlinger <pmdenlinger@icloud.com>
2025-11-26 11:09:00 -0800

    Add modifiers and padding to main design elements

--- Tax Calculator/Tax Calculator/ContentView.swift
+++ Tax Calculator/Tax Calculator/ContentView.swift
@@ -15,15 +15,25 @@ struct ContentView: View {
         VStack {
             
             Text("Annual Salary")
+                .font(.title)
+                .fontWeight(.semibold)
+                .padding(20)
             
             TextField("Salary", text: $salary)
+                .frame(width: 200.0)
+                .border(.black, width: 1)
+                .padding(20)
             
             Button(action: {
             }, label: {
                 Text("Calculate Tax")
+                    .fontWeight(.semibold)
             })
+            .buttonStyle(.borderedProminent)
+            .padding(20)
             
         }
+        .padding(100)
     }
 }
 
