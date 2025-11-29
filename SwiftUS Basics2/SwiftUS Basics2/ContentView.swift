3650e5c122737167bb95ef3d5abdbc23959793cb
Paul Denlinger <pmdenlinger@icloud.com>
2025-11-29 15:19:35 -0800

    Add activeColor var and ternary operator to change state of [Color] when tapped

--- SwiftUS Basics2/SwiftUS Basics2/ContentView.swift
+++ SwiftUS Basics2/SwiftUS Basics2/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let colorList1: [Color] = [.red, .purple, .green, .pink]
+    let colorList2: [Color] = [.black, .white, .blue]
+    
+    @State var screenTapped: Bool = false
+    
+    var activeColor: [Color] {
+        screenTapped ? colorList1 : colorList2
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        
+        ZStack {
+//            Background Color
+            LinearGradient(
+                colors: activeColor,
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .opacity(0.7)
+            .ignoresSafeArea()
+            
+//            Text
+            Text("SwiftUI Basics 2")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(.black)
+            
+            
         }
-        .padding()
+        .onTapGesture {
+            screenTapped.toggle()
+        }
     }
 }
 
