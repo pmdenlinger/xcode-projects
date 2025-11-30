479eb03faee7f52c4832bbca4bd821dea889d6c2
Paul Denlinger <pmdenlinger@icloud.com>
2025-11-29 18:48:05 -0800

    First commit. First screen layout.

---
+++ Tax Calculator 2/Tax Calculator 2/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Tax Calculator 2
+//
+//  Created by Paul Denlinger on 11/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var salary: String = ""
+    
+    var body: some View
+    {
+        VStack
+        {
+            Text( "Annual Salary" )
+            
+            TextField( "Salary", text: $salary)
+            
+        Button(action: {
+            
+        }, label: {
+            Text("Calculate Tax") }
+                )
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ Tax Calculator 2/Tax Calculator 2/Tax_Calculator_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Tax_Calculator_2App.swift
+//  Tax Calculator 2
+//
+//  Created by Paul Denlinger on 11/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Tax_Calculator_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
