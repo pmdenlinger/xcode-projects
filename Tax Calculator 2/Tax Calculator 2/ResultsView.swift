f00bcc73d727a600a910a4bf02552932ee0fc2b5
Paul Denlinger <pmdenlinger@icloud.com>
2025-11-30 10:22:44 -0800

    Import SwiftUICharts, add ResultsView page, and add import statement

---
+++ Tax Calculator 2/Tax Calculator 2/ResultsView.swift
@@ -0,0 +1,19 @@
+//
+//  ResultsView.swift
+//  Tax Calculator 2
+//
+//  Created by Paul Denlinger on 11/30/25.
+//
+
+import SwiftUI
+import SwiftUICharts
+
+struct ResultsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultsView()
+}
