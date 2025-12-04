925b7fb9b89d947d8c18117b6816ed89870b0fe6
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-04 10:00:33 -0800

    First commit. Add WeatherInfo struct.

---
+++ StaticList/StaticList/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  StaticList
+//
+//  Created by Paul Denlinger on 12/4/25.
+//
+
+import SwiftUI
+
+struct WeatherInfo: Identifiable {
+    var id: UUID()
+    var image: String
+    var temp: Int
+    var city: String
+}
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StaticList/StaticList/StaticListApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticListApp.swift
+//  StaticList
+//
+//  Created by Paul Denlinger on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct StaticListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
