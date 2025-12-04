ae388f7d53d91bf68155d5e140e287dac724b92b
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-04 13:41:32 -0800

    First commit. Create WeatherRow struct and add weatherData

---
+++ CustomRows/CustomRows/CustomRowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomRowsApp.swift
+//  CustomRows
+//
+//  Created by Paul Denlinger on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomRowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
