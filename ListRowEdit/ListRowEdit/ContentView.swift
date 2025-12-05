576184ba66eb0869e41953d6dceb2d274e20106d
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-05 09:47:10 -0800

    Copyand paste  code and add EditButton()

---
+++ ListRowEdit/ListRowEdit/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  ListRowEdit
+//
+//  Created by Paul Denlinger on 12/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var countries = ["USA", "Canada", "UK", "Germany", "France", "Italy", "China", "Japan", "India"]
+    
+    
+    var body: some View {
+        
+        NavigationStack {
+            List {
+                ForEach(countries, id: \.self) { country in
+                    Text(country)
+                }
+                .onDelete(perform: deleteItem)
+            }
+            .navigationTitle("Countries")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                EditButton()
+            }
+            
+        }
+    }
+    private func deleteItem(at indexSet: IndexSet) {
+        countries.remove(atOffsets: indexSet)
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ ListRowEdit/ListRowEdit/ListRowEditApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListRowEditApp.swift
+//  ListRowEdit
+//
+//  Created by Paul Denlinger on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ListRowEditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
