de8a19bb2a1383fac1e427aa5d11e84a9c9012d1
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-12 09:51:57 -0800

    Starting point for WeSplit app

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/WeSplit/WeSplit/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/WeSplit/WeSplit/ContentView.swift
@@ -9,30 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = "Harry"
-    
     var body: some View {
-        
-        NavigationStack {
-            
-            Form {
-                
-                Picker("Select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
-                    }
-                }
-                
-            }
-                
-        }
-        .navigationTitle(Text("WeSplit"))
-        .navigationBarTitleDisplayMode(.inline)
-            
+        Text("Hello, World!")
     }
-        
-        
 }
 
 
