74fec56e3c79bb95bdcb61fdd2cabc930a575529
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-12 09:53:53 -0800

    Add three private state vars for WeSplit

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/WeSplit/WeSplit/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/WeSplit/WeSplit/ContentView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
     var body: some View {
         Text("Hello, World!")
     }
