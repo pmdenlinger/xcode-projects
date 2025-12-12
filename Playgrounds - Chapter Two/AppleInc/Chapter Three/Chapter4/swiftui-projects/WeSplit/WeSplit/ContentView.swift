bda7643a0138f2278501e1643e7f7a7153e4b262
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-12 10:03:07 -0800

    Change text to value

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/WeSplit/WeSplit/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/WeSplit/WeSplit/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
     var body: some View {
         Form {
             Section {
-                TextField("Amount", text: $checkAmount,
+                TextField("Amount", value: $checkAmount,
                           format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
             }
         }
