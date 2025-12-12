609add23e91c195624bd3ea5b6efbbda0845a0fb
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-12 09:55:51 -0800

    Add tipPercentages Array<Int> so user can choose tip percentages

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/WeSplit/WeSplit/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/WeSplit/WeSplit/ContentView.swift
@@ -13,6 +13,8 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
         Text("Hello, World!")
     }
