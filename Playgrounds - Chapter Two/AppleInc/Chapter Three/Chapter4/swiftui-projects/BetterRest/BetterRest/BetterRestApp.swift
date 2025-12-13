000c030d8068255e9e990cc2d9cac4f413dab364
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-13 12:40:47 -0800

    Original image and text, and original image and text with .background(.red) added as modifier

---
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Paul Denlinger on 12/13/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
