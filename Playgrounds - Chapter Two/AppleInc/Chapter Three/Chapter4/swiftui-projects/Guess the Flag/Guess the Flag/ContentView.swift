503891a03ddb527760827d5c2bbc6923745bda16
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-12 20:25:39 -0800

    Change blue background to linear gradient

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Guess the Flag/Guess the Flag/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Guess the Flag/Guess the Flag/ContentView.swift
@@ -30,7 +30,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            Color.blue
+            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
             VStack(spacing: 30) {
