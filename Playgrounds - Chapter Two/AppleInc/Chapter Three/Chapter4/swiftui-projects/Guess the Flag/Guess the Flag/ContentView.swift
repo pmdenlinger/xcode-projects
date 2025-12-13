722398828467ed784d4fa2fe068f2fc87b3ad855
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-12 20:01:59 -0800

    First build of game layout

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Guess the Flag/Guess the Flag/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Guess the Flag/Guess the Flag/ContentView.swift
@@ -9,28 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var showingAlert = false
-    var body: some View {
-        VStack {
-            Button("Show Alert") {
-                showingAlert = true
-            }
-            .alert("Important message", isPresented: $showingAlert) {
-            Button("OK") {
-            }
-            }
-         
-        }
-            
-    }
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy",
+                     "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
     
-    func executeDelete() {
-        print("Performing delete")
+    var body: some View {
+        VStack(spacing: 30) {
+            
+            VStack {
+                Text("Tap the flag of")
+                Text(countries[correctAnswer])
+            }
+            
+            ForEach(0..<3) { number in
+                Button {
+                        // flag was tapped
+                } label: {
+                    Image(countries[number])
+                }
+            }
+        }
     }
 }
-
     
     #Preview {
         ContentView()
     }
+
 
