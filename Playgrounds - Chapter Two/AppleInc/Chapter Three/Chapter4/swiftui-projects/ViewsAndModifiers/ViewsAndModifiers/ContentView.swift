0e44ef5fa6db21bcf149dec05f36504c3c897585
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-13 14:03:45 -0800

    Add three state vars and NavigationStack, VStack and text, DatePicker

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -10,19 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        VStack {
-            
-           
-                Text("Hello, world!")
-                .padding()
-                .background(Color.red)
-                .padding()
-                .background(Color.blue)
-                .padding()
-                .background(Color.green)
-                .padding()
-                .background(Color.yellow)
+        Button("Hello world"){
+            print(type(of: self.body))
         }
+        .frame(width: 200, height: 200)
+        .background(Color.red)
     }
 }
 
