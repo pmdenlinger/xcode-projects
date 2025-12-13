0d817800a86cbdf07d007dbbd120c42a8272ccc6
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-12 20:05:11 -0800

    Add ZStack to wrap layout and add blue background color

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Guess the Flag/Guess the Flag/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Guess the Flag/Guess the Flag/ContentView.swift
@@ -14,18 +14,25 @@ struct ContentView: View {
     var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        VStack(spacing: 30) {
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
             
-            VStack {
-                Text("Tap the flag of")
-                Text(countries[correctAnswer])
-            }
-            
-            ForEach(0..<3) { number in
-                Button {
-                        // flag was tapped
-                } label: {
-                    Image(countries[number])
+            VStack(spacing: 30) {
+                
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                        
+                    Text(countries[correctAnswer])
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                            // flag was tapped
+                    } label: {
+                        Image(countries[number])
+                    }
                 }
             }
         }
