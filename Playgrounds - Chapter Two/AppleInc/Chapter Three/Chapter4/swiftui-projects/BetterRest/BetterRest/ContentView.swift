ed803283f7f045c8195b7af3f16e2abdc955d533
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-13 14:10:11 -0800

    Add text and stepper inputs with ranges

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/BetterRest/BetterRest/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/BetterRest/BetterRest/ContentView.swift
@@ -20,10 +20,21 @@ struct ContentView: View {
                 Text("When do you want to wake up?")
                     .font(.headline)
                 
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                DatePicker("Please enter a time", selection: $wakeUp,
+                                        displayedComponents: .hourAndMinute)
                     .labelsHidden()
                     
-                // more to come
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount,
+                        in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper("\(coffeeAmount) cup(s)", value : $coffeeAmount, in: 1...20)
+                        
 
             }
 }
