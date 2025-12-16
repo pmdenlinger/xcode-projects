5fffe456995725a591ce27901efc5a8fce0269ac
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-15 17:31:59 -0800

    Add sections and section headers (titles)

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/WordScramble/WordScramble/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/WordScramble/WordScramble/ContentView.swift
@@ -10,16 +10,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         List {
-            
-            Text("Static row 1")
-            Text("Static row 2")
-            
-            ForEach(0..<5) {
-                Text("Dynamic row \($0)")
+           
+            Section("Section 1") {
+                Text("Static row 1")
+                Text("Static row 2")
             }
             
-            Text("Static row 3")
-            Text("Static row 4")
+            Section("Section 2") {
+                ForEach(0..<5) {
+                    Text("Dynamic row \($0)")
+                }
+            }
+            
+            Section ("Section 3") {
+                Text("Static row 3")
+                Text("Static row 4")
+            }
         }
         
     }
