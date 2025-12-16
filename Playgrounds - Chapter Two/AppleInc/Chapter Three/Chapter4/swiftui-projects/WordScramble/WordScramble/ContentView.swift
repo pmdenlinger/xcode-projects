4f4f2f47a7cfc09e6a6d596efe1bacdca9ef8ac5
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-16 11:10:44 -0800

    Add startGame() function and onAppear(perform: startGame) modifier

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/WordScramble/WordScramble/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/WordScramble/WordScramble/ContentView.swift
@@ -12,6 +12,26 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     
+    func startGame() {
+            // 1. Find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+                // 2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL)
+            {
+                // 3. Split the string up into an array of strings,
+                //   separating on line breaks
+            let allWords = startWords.components(separatedBy: "\n")
+            
+                // 4. Pick a random word, or use "silkworm" as a sensible default
+            rootWord = allWords.randomElement() ?? "silkworm"
+                // 5. Clear out any previous words
+            return
+            }
+        }
+            // If we are here then there was a problem – trigger a crash and report the error
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
     func addNewWord() {
         // Lowercase and trim the word, to make sure
         // we don't add duplicate words with case differences
@@ -46,6 +66,7 @@ struct ContentView: View {
             }
             .navigationTitle("Word Scramble")
             .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
         }
         
     }
