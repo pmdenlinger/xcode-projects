2200ce1abeda7918c62d1168b57f139c0279282c
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-15 17:37:00 -0800

    Generate dynamic rows without using ForEach

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/WordScramble/WordScramble/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/WordScramble/WordScramble/ContentView.swift
@@ -9,23 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List {
-           
-            Section("Section 1") {
-                Text("Static row 1")
-                Text("Static row 2")
-            }
-            
-            Section("Section 2") {
-                ForEach(0..<5) {
-                    Text("Dynamic row \($0)")
-                }
-            }
-            
-            Section ("Section 3") {
-                Text("Static row 3")
-                Text("Static row 4")
-            }
+        List (0..<5)  {
+           Text("Dynamic row \($0)")
         }
         
     }
