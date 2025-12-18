e47bcf98f741582fc806d3079e3c7bf36793abf0
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-18 12:15:27 -0800

    Add state vars for error messages and helper methods for resolving errors

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/WordScramble/WordScramble/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/WordScramble/WordScramble/ContentView.swift
@@ -12,40 +12,94 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     func startGame() {
-            // 1. Find the URL for start.txt in our app bundle
+        // 1. Find the URL for start.txt in our app bundle
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
-                // 2. Load start.txt into a string
-            if let startWords = try? String(contentsOf: startWordsURL)
-            {
+            // 2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL) {
                 // 3. Split the string up into an array of strings,
                 //   separating on line breaks
-            let allWords = startWords.components(separatedBy: "\n")
-            
+                let allWords = startWords.components(separatedBy: "\n")
+                
                 // 4. Pick a random word, or use "silkworm" as a sensible default
-            rootWord = allWords.randomElement() ?? "silkworm"
+                rootWord = allWords.randomElement() ?? "silkworm"
                 // 5. Clear out any previous words
-            return
+                usedWords.removeAll()
+                return
             }
         }
-            // If we are here then there was a problem – trigger a crash and report the error
+        // If we are here then there was a problem – trigger a crash and report the error
         fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word,
+                                                            range: range,
+                                                            startingAt: 0,
+                                                            wrap: false,
+                                                            language: "en")
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
     
     func addNewWord() {
         // Lowercase and trim the word, to make sure
         // we don't add duplicate words with case differences
-        let answer =
-        newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-        // exit if the remaining string is empty
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Exit if the remaining string is empty
         guard answer.count > 0 else { return }
-        // extra validation to come
+        
+        // Extra validation
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original!")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
         withAnimation {
             usedWords.insert(answer, at: 0)
         }
         newWord = ""
     }
-    
+        
     var body: some View {
         NavigationStack {
             List {
@@ -60,16 +114,20 @@ struct ContentView: View {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
                         }
-                        }
                     }
                 }
             }
-            .navigationTitle("Word Scramble")
+            .navigationTitle(rootWord)
             .onSubmit(addNewWord)
             .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK") { }
+            } message: {
+                Text(errorMessage)
+            }
         }
-        
     }
+}
 
 #Preview {
     ContentView()
