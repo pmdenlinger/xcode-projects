a78dcfed67715bd3ed838b62396237b173739a8b
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-19 14:08:57 -0800

    Create animation button with scale effect

---
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Animations/Animations/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Paul Denlinger on 12/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var animationAmount = 1.0
+    
+    var body: some View {
+        VStack {
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
