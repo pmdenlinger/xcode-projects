299030b9c5cae1cfee55d336be7a4690596c26a3
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-20 14:05:35 -0800

    Add animation to button action with axis controls

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Animations/Animations/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Animations/Animations/ContentView.swift
@@ -9,19 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
     
     var body: some View {
         Button("Tap Me") {
-                // do nothing
-            
+            withAnimation {
+                animationAmount += 360
+            }
         }
         .padding(50)
         .background(.red)
         .foregroundStyle(.white)
         .clipShape(Circle())
+        .rotation3DEffect(
+            .degrees(animationAmount),
+            axis: (x: 0, y: 1, z: 0))
+        
     }
 }
+
 
 
 #Preview {
