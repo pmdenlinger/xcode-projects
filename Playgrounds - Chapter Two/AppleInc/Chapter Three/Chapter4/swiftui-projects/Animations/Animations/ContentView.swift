98d652489f67e729c5c65d30d3e98d4819db4c9a
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-20 13:41:26 -0800

    Chnage to button image

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Animations/Animations/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Animations/Animations/ContentView.swift
@@ -12,25 +12,16 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        print(animationAmount)
-        return VStack {
+        Button("Tap Me") {
+                // do nothing
             
-            
-            Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
-                
-            Spacer()
-            
-            Button("Tap Me") {
-                animationAmount += 1
-            }
-            .padding(40)
-            .background(.red)
-            .foregroundStyle(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount)
-            }
         }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(Circle())
     }
+}
 
 
 #Preview {
