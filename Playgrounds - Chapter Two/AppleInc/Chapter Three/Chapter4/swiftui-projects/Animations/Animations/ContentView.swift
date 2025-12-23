1dd492d2d1f3c3be47e8457fa96709d4f657da8e
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-23 12:30:57 -0800

    Create SwiftUI text layout and animate it the array when it is dragged across the screen

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Animations/Animations/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Animations/Animations/ContentView.swift
@@ -9,27 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     
-   @State private var dragAmount = CGSize.zero
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
     
     var body: some View {
-        LinearGradient(
-            colors: [.yellow, .red],
-            startPoint: .topLeading, endPoint: .bottomTrailing)
-        .frame(width: 300, height: 200)
-        .clipShape(.rect(cornerRadius: 10))
-        .offset(dragAmount)
-        .gesture(
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(
+                        .linear.delay(Double(num) / 20),
+                        value: dragAmount)
+            }
+        }
+        .gesture (
             DragGesture()
                 .onChanged { dragAmount = $0.translation }
                 .onEnded { _ in
-                    withAnimation(.bouncy) {
-                        dragAmount = .zero
-                    }
+                    dragAmount = .zero
+                    enabled.toggle()
                 }
-            )
-        
-        
-        
+        )
     }
 }
 
