6c298f7d22b2fce7396d0906400e5ad611c50e59
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-23 12:36:20 -0800

    Create red rect with Tap Me message

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Animations/Animations/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Animations/Animations/ContentView.swift
@@ -9,31 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let letters = Array("Hello SwiftUI")
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
+    
     
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count, id: \.self) { num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enabled ? .blue : .red)
-                    .offset(dragAmount)
-                    .animation(
-                        .linear.delay(Double(num) / 20),
-                        value: dragAmount)
+        VStack {
+            Button("Tap Me") {
+                    // Do nothing
             }
+            
+            Rectangle()
+                .fill(.red)
+                .frame(width: 200, height: 200)
         }
-        .gesture (
-            DragGesture()
-                .onChanged { dragAmount = $0.translation }
-                .onEnded { _ in
-                    dragAmount = .zero
-                    enabled.toggle()
-                }
-        )
+                
     }
 }
 
