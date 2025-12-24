02f10851abf8f7b4cf759b92b80c37833aeab877
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-24 12:02:09 -0800

    Create user struct for first commit

---
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Paul Denlinger on 12/24/25.
+//
+
+import SwiftUI
+
+struct User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
+}
+
+struct ContentView: View {
+    
+    @State private var user = User()
+    
+    var body: some View {
+        VStack {
+            Text("Your name is \(user.firstName) \(user.lastName)")
+            TextField("First Name", text: $user.firstName)
+            TextField("Last Name", text: $user.lastName)
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Paul Denlinger on 12/24/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
