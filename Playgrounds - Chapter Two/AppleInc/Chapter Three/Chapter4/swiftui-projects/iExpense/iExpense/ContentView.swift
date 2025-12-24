32e3501c53b5a3e8737e03a63eff90fb004c2b6c
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-24 13:02:53 -0800

    Add removeRows() function and .onDelete modifier to ForEach, .toolbar to add Edit Button to confirm delete

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/iExpense/iExpense/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/iExpense/iExpense/ContentView.swift
@@ -14,20 +14,29 @@ struct ContentView: View {
     @State private var numbers = [Int]()
     @State private var currentNumber = 1
     
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+    }
+    
     var body: some View {
         VStack {
             List {
                 ForEach(numbers, id: \.self) {
                     Text("Row \($0)")
                 }
+                .onDelete(perform: removeRows)
             }
             Button("Add Number") {
                 numbers.append(currentNumber)
                 currentNumber += 1
             }
         }
+        .toolbar {
+            EditButton()
+        }
     }
 }
+
                 
 
 #Preview {
