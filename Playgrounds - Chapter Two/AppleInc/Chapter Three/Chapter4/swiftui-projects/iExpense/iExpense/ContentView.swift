9865d07048815f863269ec7623bf41217277077a
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-26 09:11:37 -0800

    Add button and encoder statements to save to UserDefault

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/iExpense/iExpense/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/iExpense/iExpense/ContentView.swift
@@ -7,21 +7,28 @@
 
 import SwiftUI
 
+struct User: Codable {
+    let firstName: String
+    let lastName: String
+}
 
 struct ContentView: View {
+    
+    @State private var users = User(firstName: "Taylor", lastName: "Swift")
+    
     var body: some View {
-        
+        Button("Save User") {
+            let encoder = JSONEncoder()
+            
+            if let data = try? encoder.encode(users) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
+        }
     }
 }
-                
 
 #Preview {
     ContentView()
-}
-
-Struct User: Codable {
-    let firstName: String
-    let lastName: String
 }
         
 
