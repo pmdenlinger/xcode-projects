3812bddbaa812abf6a953df74bf66702d94b9678
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-26 08:59:30 -0800

    Add User struct

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/iExpense/iExpense/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/iExpense/iExpense/ContentView.swift
@@ -6,23 +6,22 @@
 //
 
 import SwiftUI
-import Observation
 
 
 struct ContentView: View {
-    @AppStorage("Tap") private var tapCount = 0
-    
-    
     var body: some View {
-        Button("Tap count: \(tapCount)") {
-            tapCount += 1
-        }
+        
     }
 }
                 
 
 #Preview {
     ContentView()
+}
+
+Struct User {
+    let firstName: String
+    let lastName: String
 }
         
 
