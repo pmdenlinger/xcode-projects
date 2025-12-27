4414b4ddab5aec69329e9faab5ef9487736c99a0
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-27 12:33:04 -0800

    Modify NavigationLink to show rows leading to detail pages

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Moonshot/Moonshot/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Moonshot/Moonshot/ContentView.swift
@@ -13,26 +13,15 @@ struct ContentView: View {
     var body: some View {
         
         NavigationStack {
-            NavigationLink {
-                Text("Detail View")
-            } label: {
-                VStack {
-                    Text("This is the label")
-                    Text("So is this")
-                    Image(systemName: "face.smiling")
-                }
-                .font(.largeTitle)
-                        
+            List(0..<100) { row in
+                NavigationLink("Row \(row)") {
+                    Text("Detail \(row)")
                 }
             }
-            
             .navigationTitle("SwiftUI")
-            
-                
-            }
-                
         }
-        
+    }
+}
 
 
 
