a3ce83ca2c8c177b281c4daa306779cae82acfa7
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-27 11:02:11 -0800

    Add resizable modifier and delete clipped

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Moonshot/Moonshot/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Moonshot/Moonshot/ContentView.swift
@@ -10,8 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Image("apollo11")
+            .resizable()
             .frame(width:300, height:300)
-            .clipped()
+            
             
     }
 }
