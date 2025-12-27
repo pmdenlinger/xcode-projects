86e0aae70dde12c1cca87859b5bcbd542c95210d
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-26 16:09:02 -0800

    First commit after setup

---
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Paul Denlinger on 12/26/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
