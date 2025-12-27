e6a1b6b01e015a633b454020ec0488dbeec8c141
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-27 11:17:36 -0800

    Delete Apollo 11 image code and replace it with ScrollView setup code

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Moonshot/Moonshot/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Moonshot/Moonshot/ContentView.swift
@@ -9,14 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image("apollo11")
-            .resizable()
-            .scaledToFit()
-            .containerRelativeFrame(.horizontal) {
-                size, axis in size * 0.8
+        ScrollView {
+            VStack(spacing: 10) {
+                Image("apollo11")
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) { size, axis in
+                        size * 0.8
+                    }
+                
+                ForEach(0..<100) { index in
+                    Text("Item \(index)")
+                        .font(.title)
+                }
             }
-            
-            
+        }
     }
 }
 
