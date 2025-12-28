0158e0829fccc9c86c5abdd04a2985f3603c9948
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-28 12:43:06 -0800

    Add two state properties

--- swiftui-cookbook/FormattedText/FormattedText/ContentView.swift
+++ swiftui-cookbook/FormattedText/FormattedText/ContentView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var password = "1234"
+    @State private var someText = "initial text"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, World!")
+                .fontWeight(.medium)
+                
         }
-        .padding()
+       
     }
 }
 
