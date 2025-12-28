843200f7a44426ac1b974e9a4b595f8f1b1df62f
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-28 10:26:13 -0800

    Restore code to original state to formally begin project

--- Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Moonshot/Moonshot/ContentView.swift
+++ Playgrounds - Chapter Two/AppleInc/Chapter Three/Chapter4/swiftui-projects/Moonshot/Moonshot/ContentView.swift
@@ -11,21 +11,11 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let layout = [
-        GridItem(.adaptive(minimum:80, maximum: 120)),
-        GridItem(.adaptive(minimum:80, maximum: 120)),
-        GridItem(.adaptive(minimum:80, maximum: 120))
-    ]
+    
     
     var body: some View {
         
-        ScrollView(.horizontal) {
-            LazyHGrid(rows: layout) {
-                ForEach(1..<1000) { index in
-                    Text("Item \(index)")
-                }
-            }
-        }
+        
     }
 }
 
