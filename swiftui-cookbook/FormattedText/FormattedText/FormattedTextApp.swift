dae7b54f3b648a4f14610f750b3ae5f9d6b18bdf
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-28 12:40:40 -0800

    First commit with .fontWeight

---
+++ swiftui-cookbook/FormattedText/FormattedText/FormattedTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormattedTextApp.swift
+//  FormattedText
+//
+//  Created by Paul Denlinger on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FormattedTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
