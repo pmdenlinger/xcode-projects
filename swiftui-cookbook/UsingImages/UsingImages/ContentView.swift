3f1cf93b5498a4b1f1df2b86dd2cbfaa6c62217c
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-28 13:07:01 -0800

    Add images to assets

---
+++ swiftui-cookbook/UsingImages/UsingImages/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UsingImages
+//
+//  Created by Paul Denlinger on 12/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-cookbook/UsingImages/UsingImages/UsingImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingImagesApp.swift
+//  UsingImages
+//
+//  Created by Paul Denlinger on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct UsingImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
