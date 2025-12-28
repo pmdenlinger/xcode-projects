997eccbd0e299239a7fbb6c51d581ac6f143f532
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-28 14:39:07 -0800

    Configure MenuButtonView

--- swiftui-cookbook/Buttons/Buttons/MenuButtonView.swift
+++ swiftui-cookbook/Buttons/Buttons/MenuButtonView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct MenuButtonView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Menu("Choose a country") {
+            Button("Canada") {
+                print("Canada selected")
+            }
+            Button("Mexico") {
+                print("Mexico selected")
+            }
+            Button("USA") {
+                print("USA selected")
+            }
+                
+        }
+        .navigationTitle("MenuButtons")
     }
 }
 
