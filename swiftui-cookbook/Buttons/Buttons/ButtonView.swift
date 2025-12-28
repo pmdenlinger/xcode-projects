caf48cd8fa3fa6089fca3eca3c15aeed004fe9c9
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-28 13:42:28 -0800

    Config ButtonView

--- swiftui-cookbook/Buttons/Buttons/ButtonView.swift
+++ swiftui-cookbook/Buttons/Buttons/ButtonView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct ButtonView: View {
+    
+    @State var count = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Welcome to your second view")
+            Text("Current count value: \(count)")
+                .padding()
+            Button {
+                count += 1
+            } label: {
+                Text("Tap to increment count")
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(.blue)
+                    .clipShape(Capsule())
+            }
+        }.navigationBarTitle("Button View")
     }
 }
 
