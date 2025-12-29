b0374309e1241eec9b30b2825918017482b16b8a
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-28 18:54:25 -0800

    Add BackgroundStyle: ViewModifier

--- swiftui-cookbook/UsingViewModifiers/UsingViewModifiers/ContentView.swift
+++ swiftui-cookbook/UsingViewModifiers/UsingViewModifiers/ContentView.swift
@@ -13,6 +13,18 @@ struct ContentView: View {
     }
 }
 
+struct BackgroundStyle: ViewModifier {
+    var bgColor: Color
+    func body(content: Content) -> some View {
+        content
+            .frame(width: UIScreen.main.bounds.width * 0.3)
+            .foregroundStyle(.black)
+            .padding()
+            .background(bgColor)
+            .cornerRadius(20)
+        }
+}
+
 #Preview {
     ContentView()
 }
