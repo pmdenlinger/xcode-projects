78023542d53be209c57947ba85749f315f5a8e54
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-29 13:48:35 -0800

    Add NavigationStack, List and EditButton in toolbar

--- swiftui-cookbook/chapter02/ListRowEdit/ListRowEdit/ContentView.swift
+++ swiftui-cookbook/chapter02/ListRowEdit/ListRowEdit/ContentView.swift
@@ -12,13 +12,21 @@ struct ContentView: View {
     @State private var countries = ["USA", "Canada", "Mexico", "England", "Spain", "Cameroon", "South Africa", "Japan", "South Korea"]
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(countries, id: \.self) { country in
+                    Text(country)
+                }
+                .onDelete(perform: deleteItem)
+            }
+            .navigationTitle("Countries")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar { EditButton() }
+        
         }
-        .padding()
+    }
+    private func deleteItem(atIndexSet:  IndexSet) {
+        countries.remove(atOffsets: atIndexSet)
     }
 }
 
