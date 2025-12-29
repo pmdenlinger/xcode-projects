9756bcbae48c697b6e9e336345bdad4c37ba9c8b
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-28 18:49:56 -0800

    Add text for first commit

---
+++ swiftui-cookbook/UsingViewModifiers/UsingViewModifiers/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  UsingViewModifiers
+//
+//  Created by Paul Denlinger on 12/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Perfect")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-cookbook/UsingViewModifiers/UsingViewModifiers/UsingViewModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingViewModifiersApp.swift
+//  UsingViewModifiers
+//
+//  Created by Paul Denlinger on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct UsingViewModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
