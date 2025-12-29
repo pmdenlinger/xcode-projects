5ed2cd64760947c49bfe795c3489ff455caef447
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-29 10:44:50 -0800

    Add modifiers and function with computed property outside of body

--- swiftui-cookbook/chapter02/ListRowAdd/ListRowAdd/ContentView.swift
+++ swiftui-cookbook/chapter02/ListRowAdd/ListRowAdd/ContentView.swift
@@ -17,11 +17,23 @@ struct ContentView: View {
                 ForEach(self.numbers, id: \.self) { number in
                     Text("\(number)")
                 }
-                
+            }
+            .navigationTitle("Number List")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                Button("Add") {
+                    addItemToRow()
+                }
             }
         }
+        
+    }
+    private func addItemToRow() {
+        self.numbers.append(Int.random(in: 5..<100))
+    
     }
 }
+
 
 #Preview {
     ContentView()
