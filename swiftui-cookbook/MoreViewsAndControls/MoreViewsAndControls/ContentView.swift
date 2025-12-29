df9c334e2a4cf36e453db02b5a7e35c84d7a603d
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-29 08:42:16 -0800

    Add four state properties; first commit

---
+++ swiftui-cookbook/MoreViewsAndControls/MoreViewsAndControls/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoreViewsAndControls
+//
+//  Created by Paul Denlinger on 12/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var progress = 0.5
+    @State private var color = Color.red
+    @State private var secondColor = Color.yellow
+    @State private var someText = "Initial value"
+    
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-cookbook/MoreViewsAndControls/MoreViewsAndControls/MoreViewsAndControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoreViewsAndControlsApp.swift
+//  MoreViewsAndControls
+//
+//  Created by Paul Denlinger on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct MoreViewsAndControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
