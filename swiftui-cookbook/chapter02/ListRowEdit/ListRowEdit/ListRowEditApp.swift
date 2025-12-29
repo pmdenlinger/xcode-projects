747188db22bd75a6b7f722e525d685175bdaaa74
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-29 13:43:34 -0800

    Add country array as state property

---
+++ swiftui-cookbook/chapter02/ListRowEdit/ListRowEdit/ListRowEditApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListRowEditApp.swift
+//  ListRowEdit
+//
+//  Created by Paul Denlinger on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ListRowEditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
