05c1bf7c01b070dd0d2ddc9ba6883e9e52294748
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-29 13:56:07 -0800

    Add countries array as state property

---
+++ swiftui-cookbook/chapter02/MovingListRows/MovingListRows/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  MovingListRows
+//
+//  Created by Paul Denlinger on 12/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var countries = ["USA", "Canada", "Mexico", "England", "Spain", "Cameroon", "South Africa", "Japan", "South Korea"]
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
