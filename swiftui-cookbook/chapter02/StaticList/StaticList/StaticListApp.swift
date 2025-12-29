73a4154075d369c4c1378a6d173ef90c68e413ab
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-29 09:31:46 -0800

    Add WeatherInfo struct

---
+++ swiftui-cookbook/chapter02/StaticList/StaticList/StaticListApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticListApp.swift
+//  StaticList
+//
+//  Created by Paul Denlinger on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct StaticListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
