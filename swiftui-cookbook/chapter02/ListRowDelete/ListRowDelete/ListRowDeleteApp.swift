2c1c0eb6d67134aac3e34b84c1ad666bd04b44eb
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-29 13:28:07 -0800

    Add array of countries as state property

---
+++ swiftui-cookbook/chapter02/ListRowDelete/ListRowDelete/ListRowDeleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListRowDeleteApp.swift
+//  ListRowDelete
+//
+//  Created by Paul Denlinger on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ListRowDeleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
