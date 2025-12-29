41bb6d84ed7fdaf8ef7326c88eea89c889cebe9f
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-28 18:36:09 -0800

    Add second, third sections

--- swiftui-cookbook/UsingPickers/UsingPickers/ContentView.swift
+++ swiftui-cookbook/UsingPickers/UsingPickers/ContentView.swift
@@ -12,20 +12,35 @@ struct ContentView: View {
     @State private var choice = 0
     @State private var showText = false
     @State private var transitModes = ["Bike", "Car", "Bus"]
-    @State private var sliderVal = 0
+    @State private var sliderVal: Float = 0
+    @State private var stepVal = 0
     @State private var gameTime = Date()
     
     var body: some View {
         Form {
             Section {
                 Picker("Transit Modes", selection: $choice) {
-                    ForEach(0 ..< transitModes.count, id: \.self) {
-                        Text("\("transitModes[index])")
+                    ForEach(0 ..< transitModes.count, id: \.self) { index in
+                        Text("\(transitModes[index])")
                     }
                 }
                 .pickerStyle(.segmented)
                 Text("Current choice: \(transitModes[choice])")
             }
+            Section {
+                Toggle(isOn: $showText) {
+                    Text("Show Text")
+                }
+                if showText {
+                    Text("The Text toggle is on")
+                }
+                
+            }
+            Section {
+                Slider(value: $sliderVal, in: 0...10, step: 0.001)
+                Text("Slider current value \(sliderVal, specifier: "%.1f")")
+            }
+
         }
     }
 }
