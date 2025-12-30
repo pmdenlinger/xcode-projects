bd1c2a034d448dd0c652dadc7743d3ee17c87380
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-29 19:09:52 -0800

    Enter all code for app

---
+++ swiftui-cookbook/chapter02/Chap03-ExploringAdvancedComponents/LazyStacks/LazyStacks/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  LazyStacks
+//
+//  Created by Paul Denlinger on 12/29/25.
+//
+
+import SwiftUI
+
+
+struct ListRow: View {
+    let id: Int
+    let type: String
+    init(id: Int, type: String) {
+        print("Loading \(type) item \(id)")
+        self.id = id
+        self.type = type
+    }
+        var body: some View {
+            Text("\(type) \(id)").padding()
+        }
+}
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            ScrollView(.horizontal) {
+                LazyHStack {
+                    ForEach(1...10000, id: \.self) { item in
+                        ListRow(id: item, type: "Horizontal")
+                    }
+                }
+                        
+            }.frame(height: 100, alignment: .center)
+            
+            ScrollView {
+                LazyVStack {
+                    ForEach(1...10000, id: \.self) { item in
+                        ListRow(id: item, type: "Vertical")
+                    }
+                    
+                        
+                    }
+                }
+            }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-cookbook/chapter02/Chap03-ExploringAdvancedComponents/LazyStacks/LazyStacks/LazyStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyStacksApp.swift
+//  LazyStacks
+//
+//  Created by Paul Denlinger on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct LazyStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
