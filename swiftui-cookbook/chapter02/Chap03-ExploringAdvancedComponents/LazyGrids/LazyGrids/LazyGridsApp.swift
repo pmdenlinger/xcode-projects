48932ea345fc4143d54d8d5e172bfbbffd46edd9
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-30 11:08:46 -0800

    Add VStack, ScrollView to body

---
+++ swiftui-cookbook/chapter02/Chap03-ExploringAdvancedComponents/LazyGrids/LazyGrids/LazyGridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyGridsApp.swift
+//  LazyGrids
+//
+//  Created by Paul Denlinger on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct LazyGridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
