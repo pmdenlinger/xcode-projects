39db73685ed59d8e4d9cf6faa999a53686c40133
Paul Denlinger <pmdenlinger@icloud.com>
2025-12-30 12:23:34 -0800

    Add List of bagContents and children constants

--- swiftui-cookbook/chapter02/Chap03-ExploringAdvancedComponents/ExpandingLists/ExpandingLists/ContentView.swift
+++ swiftui-cookbook/chapter02/Chap03-ExploringAdvancedComponents/ExpandingLists/ExpandingLists/ContentView.swift
@@ -14,16 +14,26 @@ struct Backpack: Identifiable {
     var content: [Backpack]?
 }
 
+let dollar = Backpack(name: "Dollar", icon: "dollarsign.circle")
+let yen = Backpack(name: "Yen", icon: "yensign.circle")
+let currencies = Backpack(name: "Currencies", icon: "coloncurrencysign.circle", content: [dollar, yen])
+
+let pencil = Backpack(name: "Pencil", icon: "pencil.circle")
+let hammer = Backpack(name: "Hammer", icon: "hammer")
+let paperClip = Backpack(name: "Paperclip", icon: "paperclip")
+let glass = Backpack(name: "Magnifying glass", icon: "magnifyingglass")
+let bin  = Backpack(name: "Bin", icon: "arrow.up.bin", content: [paperClip, glass])
+let tools = Backpack(name: "Tools", icon: "folder", content: [pencil, hammer, bin])
+
 
 struct ContentView: View {
+    let bagContents = [currencies, tools]
+        // more code coming here
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(bagContents, children: \.content) {
+            row in
+            Label(row.name, systemImage: row.icon)
         }
-        .padding()
     }
 }
 
