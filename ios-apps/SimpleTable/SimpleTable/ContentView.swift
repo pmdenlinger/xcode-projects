78b61a7a541c49985dee04539bc9aeab632c13c5
Paul Denlinger <pmdenlinger@icloud.com>
2026-01-05 16:52:49 -0800

    Add restaurantNames array and modify List to display names

--- ios-apps/SimpleTable/SimpleTable/ContentView.swift
+++ ios-apps/SimpleTable/SimpleTable/ContentView.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var restaurantNames = [
+        "Cafe Deadend",
+        "Homei",
+        "Teakha",
+        "Cafe Loisl",
+        "Petite Oyster",
+        "For Kee Restaurant",
+        "Po's Atelier",
+        "Bourke Street Bakery",
+        "Haigh's Chocolate",
+        "Palomino Espresso",
+        "Upstate",
+        "Traif",
+        "Graham Avenue Meats and Deli",
+        "Waffle and Wolf",
+        "Five Leaves",
+        "Cafe Lore",
+        "Confessional",
+        "Barrafina",
+        "Donostia",
+        "Royal Oak",
+        "CASK Pub and Kitchen"
+    ]
+    
     var body: some View {
-        List(1...4, id: \.self) {
-                Text("Item \($0)")
+        
+        List{
+            ForEach(1...restaurantNames.count-1, id: \.self) { index in
+                Text(restaurantNames[index])
             }
         }
     }
-
-#Preview {
-    ContentView()
 }
+    
+    #Preview {
+        ContentView()
+    }
+
