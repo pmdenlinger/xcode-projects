0d0b7ca61684e62c152e9e6eb278f036d5189b66
Paul Denlinger <pmdenlinger@icloud.com>
2026-01-05 16:23:48 -0800

    Create simple list with four items

---
+++ ios-apps/SimpleTable/SimpleTable/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SimpleTable
+//
+//  Created by Paul Denlinger on 1/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            Text("Item 1")
+            Text("Item 2")
+            Text("Item 3")
+            Text("Item 4")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-apps/SimpleTable/SimpleTable/SimpleTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTableApp.swift
+//  SimpleTable
+//
+//  Created by Paul Denlinger on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
