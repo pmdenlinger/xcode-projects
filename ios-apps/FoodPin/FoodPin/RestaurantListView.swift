174e7e9ca2024c36b1e4ec5e3c2be7e916ad9895
Paul Denlinger <pmdenlinger@icloud.com>
2026-01-08 15:56:57 -0800

    Added view protocol to BasicTextImageRow struct

--- ios-apps/FoodPin/FoodPin/RestaurantListView.swift
+++ ios-apps/FoodPin/FoodPin/RestaurantListView.swift
@@ -7,37 +7,39 @@
 
 import SwiftUI
 
-struct BasicTextImageRow {
+struct BasicTextImageRow: View {
     
     var imageName: String
     var name: String
     var type: String
     var location: String
     
-    var body: some View {
-        
-        HStack(alignment: .top, spacing: 20) {
-            Image(restaurantImages[index])
-                .resizable()
-                .frame(width: 120, height: 118)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-            
-            VStack(alignment: .leading) {
-                Text(restaurantNames[index])
-                    .font(.system(.title2, design: .rounded))
-                
-                Text("Types")
-                    .font(.system(.body, design: .rounded))
-                
-                Text("Locations")
-                    .font(.system(.subheadline, design: .rounded))
-                    .foregroundStyle(.gray)
-                
-            }
-            
-            
-        }
-    }
+//    The code below was suggested by Copilot and should be checked against Simon //Ng's code before proceeding.
+    
+//    var body: some View {
+//        
+//            HStack(spacing: 12) {
+//                Image(imageName)
+//                    .resizable()
+//                    .scaledToFill()
+//                    .frame(width: 60, height: 60)
+//                    .cornerRadius(8)
+//                    .clipped()
+//                
+//                VStack(alignment: .leading, spacing: 4) {
+//                    Text(name)
+//                        .font(.headline)
+//                    Text(type)
+//                        .font(.subheadline)
+//                        .foregroundColor(.secondary)
+//                    Text(location)
+//                        .font(.caption)
+//                        .foregroundColor(.secondary)
+//                }
+//            }
+//            .padding(.vertical, 8)
+//        
+//    }
 }
 
 struct RestaurantListView: View {
@@ -90,58 +92,61 @@ struct RestaurantListView: View {
         "cask"
     ]
     
-//    var restaurantTypes = [
-//        "Coffee & Tea Shop",
-//        "Cafe",
-//        "Tea House",
-//        "Austrian / Casual Drink",
-//        "French",
-//        "Bakery",
-//        "Bakery",
-//        "Chocolate",
-//        "Cafe",
-//        "American / Seafood",
-//        "American",
-//        "American",
-//        "Breakfast & Brunch",
-//        "Coffee & Tea",
-//        "Coffee & Tea",
-//        "Latin American",
-//        "Spanish",
-//        "Spanish",
-//        "Spanish",
-//        "British",
-//        "Thai"
-//    ]
+    var restaurantLocations = [
+        "Hong Kong",
+        "Hong Kong",
+        "Hong Kong",
+        "Hong Kong",
+        "Hong Kong",
+        "Hong Kong",
+        "Hong Kong",
+        "Sydney",
+        "Sydney",
+        "Sydney",
+        "New York",
+        "New York",
+        "New York",
+        "New York",
+        "New York",
+        "New York",
+        "New York",
+        "London",
+        "London",
+        "London",
+        "London"
+    ]
     
-//    var restaurantLocations = [
-//        "Hong Kong",
-//        "Hong Kong",
-//        "Hong Kong",
-//        "Hong Kong",
-//        "Hong Kong",
-//        "Hong Kong",
-//        "Hong Kong",
-//        "Sydney",
-//        "Sydney",
-//        "Sydney",
-//        "New York",
-//        "New York",
-//        "New York",
-//        "New York",
-//        "New York",
-//        "New York",
-//        "New York",
-//        "London",
-//        "London",
-//        "London",
-//        "London"
-//    ]
+    var restaurantTypes = [
+        "Coffee & Tea Shop",
+        "Cafe", "Tea House",
+        "Austrian / Casual Drink",
+        "French",
+        "Bakery",
+        "Bakery",
+        "Chocolate",
+        "Cafe",
+        "American / Seafood",
+        "American",
+        "American",
+        "Breakfast & Brunch",
+        "Coffee & Tea",
+        "Coffee & Tea",
+        "Latin American",
+        "Spanish",
+        "Spanish",
+        "Spanish",
+        "British",
+        "Thai"
+    ]
     
     var body: some View {
         List {
             ForEach(restaurantNames.indices, id: \.self) {
                 index in
+                BasicTextImageRow(imageName: restaurantImages[index],
+                                  name: restaurantNames[index],
+                                  type: restaurantTypes[index],
+                                  location: restaurantLocations[index])
                 
             }
             .listRowSeparator(.hidden)
