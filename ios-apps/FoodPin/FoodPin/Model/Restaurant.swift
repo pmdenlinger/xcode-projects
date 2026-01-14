9166a76a45a3928ef2ea2a4bb1653c6dca8a21ca
Paul Denlinger <pmdenlinger@icloud.com>
2026-01-14 15:35:29 -0800

    Create Model, View folders to hold files

---
+++ ios-apps/FoodPin/FoodPin/Model/Restaurant.swift
@@ -0,0 +1,36 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by Paul Denlinger on 1/14/26.
+//
+
+import Foundation
+
+struct Restaurant {
+    var name: String
+    var type: String
+    var location: String
+    var image: String
+    var isFavorite: Bool
+    
+    init(name: String,
+         type: String,
+         location: String,
+         image: String,
+         isFavorite: Bool) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.image = image
+        self.isFavorite = isFavorite
+    }
+    
+    init() {
+        self.init(name: "",
+                  type: "",
+                  location: "",
+                  image: "",
+                  isFavorite: false)
+    }
+}
