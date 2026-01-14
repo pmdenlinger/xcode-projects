b4228a8551b4ae49961cd1a0fdcea438097a0f8e
Paul Denlinger <pmdenlinger@icloud.com>
2026-01-14 14:45:32 -0800

    Delete arrays of restaurant objects in RestaurantListView

--- ios-apps/FoodPin/FoodPin/RestaurantListView.swift
+++ ios-apps/FoodPin/FoodPin/RestaurantListView.swift
@@ -98,102 +98,15 @@ struct FullImageRow: View {
 
 struct RestaurantListView: View {
     
-    var restaurantNames = [
-        "Cafe Deadend",
-        "Homei",
-        "Teakha",
-        "Cafe Loisl",
-        "Petite Oyster",
-        "For Kee Restaurant",
-        "Po's Atelier",
-        "Bourke Street Bakery",
-        "Haigh's Chocolate",
-        "Palomino Espresso",
-        "Upstate",
-        "Traif",
-        "Graham Avenue Meats and Deli",
-        "Waffle and Wolf",
-        "Five Leaves",
-        "Cafe Lore",
-        "Confessional",
-        "Barrafina",
-        "Donostia",
-        "Royal Oak",
-        "CASK Pub and Kitchen"
-    ]
     
-    var restaurantImages = [
-        "cafedeadend",
-        "homei",
-        "teakha",
-        "cafeloisl",
-        "petiteoyster",
-        "forkee",
-        "posatelier",
-        "bourkestreetbakery",
-        "haigh",
-        "palomino",
-        "upstate",
-        "traif",
-        "graham",
-        "waffleandwolf",
-        "fiveleaves",
-        "cafelore",
-        "confessional",
-        "barrafina",
-        "donostia",
-        "royaloak",
-        "cask"
-    ]
     
-    var restaurantLocations = [
-        "Hong Kong",
-        "Hong Kong",
-        "Hong Kong",
-        "Hong Kong",
-        "Hong Kong",
-        "Hong Kong",
-        "Hong Kong",
-        "Sydney",
-        "Sydney",
-        "Sydney",
-        "New York",
-        "New York",
-        "New York",
-        "New York",
-        "New York",
-        "New York",
-        "New York",
-        "London",
-        "London",
-        "London",
-        "London"
-    ]
     
-    var restaurantTypes = [
-        "Coffee & Tea Shop",
-        "Cafe", "Tea House",
-        "Austrian / Casual Drink",
-        "French",
-        "Bakery",
-        "Bakery",
-        "Chocolate",
-        "Cafe",
-        "American / Seafood",
-        "American",
-        "American",
-        "Breakfast & Brunch",
-        "Coffee & Tea",
-        "Coffee & Tea",
-        "Latin American",
-        "Spanish",
-        "Spanish",
-        "Spanish",
-        "British",
-        "Thai"
-    ]
     
-    @State var restaurantIsFavorites = Array(repeating: false, count: 21)
+    
+    
+    
+    
+    
     
     
     var body: some View {
