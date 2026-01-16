449f291fe226a8d00d7892a31c7320a369039533
Paul Denlinger <pmdenlinger@icloud.com>
2026-01-15 19:54:23 -0800

    Modify code for Restaurant, RestaurantListView and RestaurantDetailView so that it matches with Simon Ng's at beginning of chapter 12

---
+++ .github/instructions/Swift.instructions.md
@@ -0,0 +1,5 @@
+Am learning Swift so I would like explanations and examples of Swift behavior. Include code examples when appropriate. DO NOT make changes to the code but make recommendations so that I can change the code myself.
+---
+applyTo: 'FoodPin'
+---
+Provide project context and coding guidelines that AI should follow when generating code, or answering questions.
--- ios-apps/FoodPin/FoodPin/View/RestaurantDetailView.swift
+++ ios-apps/FoodPin/FoodPin/View/RestaurantDetailView.swift
@@ -51,10 +51,6 @@ struct RestaurantDetailView: View {
 
 
 #Preview {
-    RestaurantDetailView(restaurant: Restaurant(
-                                     name: "Cafe Deadend",
-                                     type: "Cafe",
-                                     location: "Hong Kong",
-                                     image: "cafedeadend",
-                                     isFavorite: true))
+    RestaurantDetailView(restaurant: Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location: "G/F, 72 Po Hing Fong, Sheung Wan, Hong Kong", phone: "232-923423", description: "Searching for great breakfast eateries and coffee? This place is for you. We open at 6:30 every morning, and close at 9 PM. We offer espresso and espresso based drink, such as capuccino, cafe latte, piccolo and many more. Come over and enjoy a great meal.", image: "cafedeadend", isFavorite: true))
 }
+
