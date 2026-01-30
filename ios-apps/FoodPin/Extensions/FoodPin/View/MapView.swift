946656cd374eb22552ffbd315a34504c077e8336
Paul Denlinger <pmdenlinger@icloud.com>
2026-01-29 21:33:53 -0800

    Added code for reverse geocoding

--- ios-apps/FoodPin/Extensions/FoodPin/View/MapView.swift
+++ ios-apps/FoodPin/Extensions/FoodPin/View/MapView.swift
@@ -1,9 +1,9 @@
-//
-//  MapView.swift
-//  FoodPin
-//
-//  Created by Paul Denlinger on 1/29/26.
-//
+    //
+    //  MapView.swift
+    //  FoodPin
+    //
+    //  Created by Paul Denlinger on 1/29/26.
+    //
 
 import SwiftUI
 import MapKit
@@ -11,10 +11,35 @@ import MapKit
 struct MapView: View {
     var location: String = ""
     
-    @State private var region:
-    MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.510357, longitude: -0.116773), span: MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0))
+    @State private var region: MKCoordinateRegion = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 51.510357, longitude: -0.116773),
+        span: MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0)
+    )
+    
     var body: some View {
         Map(initialPosition: .region(region))
+            .task {
+                await convertAddress(location: location)
+            }
+    }
+    
+    private func convertAddress(location: String) async {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = location
+        
+        let localSearch = MKLocalSearch(request: searchRequest)
+        
+        do {
+            let response = try await localSearch.start()
+            if let coordinate = response.mapItems.first?.placemark.coordinate {
+                region = MKCoordinateRegion(
+                    center: coordinate,
+                    span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                )
+            }
+        } catch {
+            print("Search error: \(error)")
+        }
     }
 }
 
